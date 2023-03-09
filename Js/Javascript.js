4d48f07864d6ae58f948b0785c483777f3b28d73
Onni06 <onni.suominen@gmail.com>
2023-03-09 13:42:48 +0200

    Javascriptiä

---
+++ Js/Javascript.js
@@ -0,0 +1,19 @@
+
+var name = "Onni"
+ var age = 16
+var city = "Lohja"
+var study = "software developer"
+ 
+ 
+ var intro ="My name is " + name + " and i am " + age + " years old. My home city is " + city + " and i am studying to become " + study;
+
+ alert(intro)
+
+console.warn("Warning");
+console.log("Name;", name);
+console.error("Error Error");
+
+
+var test = document.getElementById("test");
+var oldText = test.innerHTML           
+test.innerHTML = "Hi welcome to Javascript for beginners";
